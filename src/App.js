ead6fc19feff7fa76ed6bd2c59095e70f3fe0c09
George Joseph Brown <georgejosephbrown@Georges-iMac.local>
2021-12-04 14:34:47 -0700

    import useRef, pdf components from kendo, test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import { useRef } from 'react';
 import './App.css';
 import '@progress/kendo-theme-material/dist/all.css';
 import { Button } from "@progress/kendo-react-buttons";
+import { PDFExport, savePDF } from "@progress/kendo-react-pdf";
 
 
 function App() {
+
+  const pdfExportComponent = useRef(null);
+
+  const handleExportWithComponent = (e) => {
+    pdfExportComponent.current.save();
+  }
+
   return (
     <div className="app-content">
+      <PDFExport ref={pdfExportComponent} paperSize="A4">
       <h1>PDF Generator</h1>
       <p>This is gonna be so cool, you guys!</p>
       <div className="button-area">
-        <Button primary={true}>Primary Button</Button>
-        <Button>Default Button</Button>
+        <Button primary={true} onClick={handleExportWithComponent}>Primary Button</Button>
       </div>
+      </PDFExport>
     </div>
   );
 }
