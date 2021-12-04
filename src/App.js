30392dd9ee83e44f98569e8d7e1fd98be1f02a23
George Joseph Brown <georgejosephbrown@Georges-iMac.local>
2021-12-04 14:46:55 -0700

    figure out exporting with method instead of component

--- src/App.js
+++ src/App.js
@@ -8,19 +8,27 @@ import { PDFExport, savePDF } from "@progress/kendo-react-pdf";
 function App() {
 
   const pdfExportComponent = useRef(null);
+  const contentArea = useRef(null);
 
   const handleExportWithComponent = (e) => {
     pdfExportComponent.current.save();
   }
 
+  const handleExportWithMethod = (e) => {
+    savePDF(contentArea.current, { paperSize: "A4" })
+  }
+
   return (
     <div className="app-content">
       <PDFExport ref={pdfExportComponent} paperSize="A4">
-      <h1>PDF Generator</h1>
-      <p>This is gonna be so cool, you guys!</p>
-      <div className="button-area">
-        <Button primary={true} onClick={handleExportWithComponent}>Primary Button</Button>
-      </div>
+        <div ref={contentArea}>
+          <h1>PDF Generator</h1>
+          <p>This is gonna be so cool, you guys!</p>
+          <div className="button-area">
+            <Button primary={true} onClick={handleExportWithComponent}>Primary Button</Button>
+            <Button onClick={handleExportWithMethod}>Export with Method</Button>
+          </div>
+        </div>
       </PDFExport>
     </div>
   );
